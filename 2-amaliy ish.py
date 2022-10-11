f05e973fac3219ec381bceb2f53b6351f4577acd
Boltaboyev Murodjon 9-ATT-21 <105468733+BoltaboyevMurodjon@users.noreply.github.com>
2022-10-11 12:33:12 +0300

    Create 2-amaliy ish.py

---
+++ 2-amaliy ish.py
@@ -0,0 +1,7 @@
+# Uchta musbat son bеrilgan. Sonlar o’rta gеomеtrigining topilsin.
+A = int(input("A="))
+B = int(input("B="))
+C = int(input("C="))
+d = (A+B+C)**(1/3)
+
+print("Kiritilgan sonlarning o`rta geometrigi: =",d,"ga teng")
