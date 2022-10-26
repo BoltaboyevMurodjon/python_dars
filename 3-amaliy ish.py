d1fb3258edfe840edc388281253198b9b5f9fe0b
Boltaboyev Murodjon 9-ATT-21 <105468733+BoltaboyevMurodjon@users.noreply.github.com>
2022-10-26 07:36:09 +0300

    Create 3-amaliy ish.py

---
+++ 3-amaliy ish.py
@@ -0,0 +1,15 @@
+a = int(input("a="))
+b = int(input("b="))
+c = int(input("c="))
+if a>0:
+    print("musbat son")
+if a<0:
+    print("manfiy son")
+if b>0:
+    print("musbat son")
+if b<0:
+    print("manfiy son")
+if c>0:
+    print("musbat son")
+if c<0:
+    print("manfiy son")
