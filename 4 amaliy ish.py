21ac74005afe6012c609c2a2693c41d612fe6a51
Boltaboyev Murodjon 9-ATT-21 <105468733+BoltaboyevMurodjon@users.noreply.github.com>
2022-12-07 15:27:05 +0500

    Update 4 amaliy ish.py

--- 4 amaliy ish.py
+++ 4 amaliy ish.py
@@ -7,3 +7,13 @@ s=0
 for m in range(0,k+1):
     s+=(n+m)**2
 print(s)
+
+# 4.2 misol
+
+toplam = {2.3,5.9,6.6,6.3}
+s=0
+for n in toplam:
+    k=(round(n))
+    s += k
+    print(k)
+print(f"yigindisi {s}") 
